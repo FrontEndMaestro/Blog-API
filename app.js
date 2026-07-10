8290b6b7b248c9e570fd4903be23f890967a93ce
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-07-10 14:57:48 +0500

    feat: add middleware to parse json and form data

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const express = require("express");
+import express from "express";
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, (error) => {
   if (error) {
