f4d8606df4d5160ba51da88f442e05f991cc3088
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-07-10 14:55:48 +0500

    feat(db): add methods to create & find users

---
+++ models/user.js
@@ -0,0 +1,34 @@
+import { prisma } from "../lib/prisma";
+
+async function createUser(userData) {
+  const user = await prisma.user.create({
+    data: {
+      name: userData.name,
+      email: userData.email,
+      password: userData.password,
+    },
+  });
+  return user;
+}
+
+async function findUserByName(name) {
+  const user = await prisma.user.findUnique({
+    where: {
+      name,
+    },
+  });
+  return user;
+}
+
+async function findUserByEmail(email) {
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  return user;
+}
+
+export { createUser, findUserByName };
+
+
