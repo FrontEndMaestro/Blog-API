1d8e8894a70c9f376dab4b2a172097850963c308
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-07-11 20:18:17 +0500

    feat(app): add routing and error middleware

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 import express from "express";
+import { userRouter } from "./routes/user.js";
+import { authRouter } from "./routes/auth.js";
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/user", userRouter);
+app.use("/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: "Something broke",
+  });
+});
 
 app.listen(3000, (error) => {
   if (error) {
